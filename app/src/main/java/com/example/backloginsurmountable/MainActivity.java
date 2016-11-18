e64348f8db56e604a6372a7db0af2d27ded33d84
Alaina Traxler <eloavox@gmail.com>
2016-11-18 09:42:32 -0800

    implement OnClickListener interface in Main and attach nehavior for backlog button

--- app/src/main/java/com/example/backloginsurmountable/MainActivity.java
+++ app/src/main/java/com/example/backloginsurmountable/MainActivity.java
@@ -1,18 +1,26 @@
 package com.example.backloginsurmountable;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 import static android.graphics.Typeface.*;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @Bind(R.id.textView_Backlog) TextView mTextView_Backlog;
     @Bind(R.id.textView_Insurmountable) TextView mTextView_Insurmountable;
+
+    @Bind(R.id.button_Backlog) Button mButton_Backlog;
+    @Bind(R.id.button_Gauntlet) Button mButton_Gauntlet;
+    @Bind(R.id.button_Profile) Button mButton_Profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +31,18 @@ public class MainActivity extends AppCompatActivity {
         Typeface nintender = createFromAsset(getAssets(), "fonts/Nintender.ttf");
         mTextView_Backlog.setTypeface(nintender);
         mTextView_Insurmountable.setTypeface(nintender);
+
+        mButton_Backlog.setOnClickListener(this);
+        mButton_Gauntlet.setOnClickListener(this);
+        mButton_Profile.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mButton_Backlog){
+            Intent intent = new Intent(MainActivity.this, BacklogActivity.class);
+            startActivity(intent);
+        }
+    }
+
 }
