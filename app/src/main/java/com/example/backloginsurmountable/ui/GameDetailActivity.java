d222a4cb8a4e0e9cefec003c3ff3de9788631d4f
Alaina Traxler <eloavox@gmail.com>
2016-12-01 09:02:31 -0800

    add GameFragment and GameDetail files.

---
+++ app/src/main/java/com/example/backloginsurmountable/ui/GameDetailActivity.java
@@ -0,0 +1,15 @@
+package com.example.backloginsurmountable.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.backloginsurmountable.R;
+
+public class GameDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_detail);
+    }
+}
