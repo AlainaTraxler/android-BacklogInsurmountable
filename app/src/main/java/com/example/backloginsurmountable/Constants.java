fa7e16e272ab0ebc4ed9a1527283edc2a2fe9ceb
Alaina Traxler <eloavox@gmail.com>
2016-12-01 11:01:05 -0800

    hook API key into constants.

---
+++ app/src/main/java/com/example/backloginsurmountable/Constants.java
@@ -0,0 +1,8 @@
+package com.example.backloginsurmountable;
+
+/**
+ * Created by Guest on 12/1/16.
+ */
+public class Constants {
+    public static final String GIANTBOMB_API_KEY = BuildConfig.GIANTBOMB_API_KEY;
+}
