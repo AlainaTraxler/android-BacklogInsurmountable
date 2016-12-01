e2cf4a69bb9bfc16b6e325e8a721fbd4bd7f305f
Alaina Traxler <eloavox@gmail.com>
2016-12-01 09:24:02 -0800

    add GameDetailFragment and backend to build it.

---
+++ app/src/main/java/com/example/backloginsurmountable/adapters/GamePagerAdapter.java
@@ -0,0 +1,7 @@
+package com.example.backloginsurmountable.adapters;
+
+/**
+ * Created by Guest on 12/1/16.
+ */
+public class GamePagerAdapter {
+}
--- app/src/main/java/com/example/backloginsurmountable/ui/GameDetailFragment.java
+++ app/src/main/java/com/example/backloginsurmountable/ui/GameDetailFragment.java
@@ -6,25 +6,53 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.backloginsurmountable.R;
+import com.example.backloginsurmountable.models.Game;
+
+import org.parceler.Parcels;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class GameDetailFragment extends Fragment {
+    @Bind(R.id.textView_Name) TextView mTextView_Name;
+    @Bind(R.id.textView_Genre) TextView mTextView_Genre;
 
+    private Game mGame;
 
     public GameDetailFragment() {
         // Required empty public constructor
     }
 
+    public static GameDetailFragment newInstance(Game game) {
+        GameDetailFragment gameDetailFragment = new GameDetailFragment();
+        Bundle args = new Bundle();
+        args.putParcelable("game", Parcels.wrap(game));
+        gameDetailFragment.setArguments(args);
+        return gameDetailFragment;
+    }
+
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game_detail, container, false);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mGame = Parcels.unwrap(getArguments().getParcelable("Game"));
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_game_detail, container, false);
+        ButterKnife.bind(this, view);
+
+        mTextView_Name.setText(mGame.getName());
+        mTextView_Genre.setText(mGame.getGenre());
+
+        return view;
     }
 
 }
