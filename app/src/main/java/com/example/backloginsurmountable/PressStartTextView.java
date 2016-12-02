a5a173268b13e24590717cc33beee95f47a1d709
Alaina Traxler <eloavox@gmail.com>
2016-12-02 11:09:34 -0800

    style InfoActivity and add implicit intent for email on contact page.

---
+++ app/src/main/java/com/example/backloginsurmountable/PressStartTextView.java
@@ -0,0 +1,33 @@
+package com.example.backloginsurmountable;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by Guest on 12/2/16.
+ */
+public class PressStartTextView extends TextView {
+    public PressStartTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    public PressStartTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public PressStartTextView(Context context) {
+        super(context);
+        init();
+    }
+
+    private void init() {
+//        if (!isInEditMode()) {
+            Typeface tf = Typeface.createFromAsset(getContext().getAssets(), "PressStart2P.ttf");
+            setTypeface(tf);
+//        }
+    }
+}
--- app/src/main/java/com/example/backloginsurmountable/ui/InfoActivity.java
+++ app/src/main/java/com/example/backloginsurmountable/ui/InfoActivity.java
@@ -1,15 +1,60 @@
 package com.example.backloginsurmountable.ui;
 
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.example.backloginsurmountable.PressStartTextView;
 import com.example.backloginsurmountable.R;
 
-public class InfoActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+import static android.graphics.Typeface.createFromAsset;
+
+public class InfoActivity extends AppCompatActivity implements View.OnClickListener{
+    @Bind(R.id.textView1) TextView mTextView1;
+    @Bind(R.id.textView2) TextView mTextView2;
+    @Bind(R.id.textView3) TextView mTextView3;
+    @Bind(R.id.textView4) TextView mTextView4;
+    @Bind(R.id.textView5) TextView mTextView5;
+    @Bind(R.id.textView6) TextView mTextView6;
+    @Bind(R.id.textView7) TextView mTextView7;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+        ButterKnife.bind(this);
+
+        Typeface PressStart2P = createFromAsset(getAssets(), "fonts/PressStart2P.ttf");
+        mTextView1.setTypeface(PressStart2P);
+        mTextView2.setTypeface(PressStart2P);
+        mTextView3.setTypeface(PressStart2P);
+        mTextView4.setTypeface(PressStart2P);
+        mTextView5.setTypeface(PressStart2P);
+        mTextView6.setTypeface(PressStart2P);
+        mTextView7.setTypeface(PressStart2P);
+
+        mTextView5.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mTextView5) {
+            Intent emailIntent = new Intent(Intent.ACTION_SEND);
+            emailIntent.setData(Uri.parse("mailto:eloavox@gmail.com"));
+            emailIntent.setType("text/plain");
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, "From App:");
+            emailIntent.putExtra(Intent.EXTRA_TEXT, "");
+            startActivity(emailIntent);
+        }
     }
 }
