ffdd4df16154560e29e1412f2236c8005a76ccda
Alaina Traxler <eloavox@gmail.com>
2016-12-12 08:33:42 -0800

    add adapters and listeners to prepare for gesture implementation.

---
+++ app/src/main/java/com/example/backloginsurmountable/utils/ItemTouchHelperAdapter.java
@@ -0,0 +1,9 @@
+package com.example.backloginsurmountable.utils;
+
+/**
+ * Created by Guest on 12/12/16.
+ */
+public interface ItemTouchHelperAdapter {
+//    boolean onItemMove(int fromPosition, int toPosition);
+    void onItemDismiss(int position);
+}
